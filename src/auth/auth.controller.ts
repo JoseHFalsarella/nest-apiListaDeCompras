6ae2aadc7f3732421a45771b5fa157e4b5902426
Jose Henrique Falsarella Lima Oliveira <joseh.oliveira0329@gmail.com>
2024-05-01 03:24:29 -0300

    feat - Criação do método POST para login e verificação das credenciais

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from 'src/DTO/register-user.dto';
+import { UserLoginDto } from 'src/DTO/user-login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,10 @@ export class AuthController {
     @Post('register')
     registration(@Body(ValidationPipe) regDto: RegisterUserDto){
         return this.authService.registerUser(regDto);
+    }
+
+    @Post('login')
+    signin(@Body(ValidationPipe) loginDto: UserLoginDto){
+        return this.authService.loginUser(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterUserDto } from 'src/DTO/register-user.dto';
 import { UserEntity } from 'src/Entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import { UserLoginDto } from 'src/DTO/user-login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>) {
+    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>,
+    private jwt: JwtService) {
     }
 
     async registerUser(registerDto: RegisterUserDto){
@@ -28,5 +31,25 @@ export class AuthService {
             throw new InternalServerErrorException('Algo deu errrado.');
         }
         
+    }
+
+    async loginUser(userLoginDto: UserLoginDto) {
+        const {username, password} = userLoginDto;
+
+        const user = await this.repo.findOne({ where: { username: username } });
+        
+        if(!user) {
+            throw new UnauthorizedException('Credenciais Inválidas.');
+        }
+        const salt = user.salt;
+        const isPassword = await bcrypt.compare(password, user.password);
+
+        if(isPassword) {
+            const jwtPayload = {username};
+            const jwtToken = await this.jwt.signAsync(jwtPayload, {expiresIn: '1d', algorithm: 'HS512'});
+            return {token: jwtToken};
+        } else {
+            throw new UnauthorizedException('Credenciais Inválidas.');
+        }
     }
 }

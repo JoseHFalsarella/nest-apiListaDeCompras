f48778562f354595e8bab081c3bb41d0c7d218b9
Jose Henrique Falsarella Lima Oliveira <joseh.oliveira0329@gmail.com>
2024-05-01 02:22:10 -0300

    feat - Implementação da DTO para registro de usuário

---
+++ src/DTO/register-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
+
+export class RegisterUserDto {
+    @IsNotEmpty()
+    username: string;
+    @IsNotEmpty()
+    @MinLength(6) @MaxLength(20)
+    @Matches(/(?:(?=.*\d)|(?=.*\W))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: "A senha deve conter ao menos um número, uma letra maiúscula e 6 ou mais caracteres."
+    })
+    password: string;
+}

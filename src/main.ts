8ef5f12f1832e2098bfa9cac3176b6ad277099b2
Jose Henrique Falsarella Lima Oliveira <joseh.oliveira0329@gmail.com>
2024-05-05 20:59:51 -0300

    Habilitação do CORS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.enableCors({
-    origin: ["http://localhost:4200"]
+    //origin: ['http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
+  await app.listen(3000);
 }
 bootstrap();
